eec72ec438e120840128f3b23075186a9690eae7
Angelo Perini <aperini351@gmail.com>
2024-10-30 20:08:08 -0300

    -------

--- Sistema de tarefas/Models/UsuarioModel.cs
+++ Sistema de tarefas/Models/UsuarioModel.cs
@@ -6,7 +6,7 @@ namespace Sistema_de_tarefas.Models
     public class UsuarioModel
     {
         [Key]
-        public required int Id { get; set; }
+        public required int? Id { get; set; }
         public required string? Nome { get; set; }
         public required string? Email { get; set; }
         public required string Senha { get; set; }
